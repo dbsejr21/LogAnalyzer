3d8b260537586e4ab25c096f1e788bd35c38264a
dbsejr21 <juyoondeok@juyundeog-ui-MacBook-Air.local>
2015-02-04 03:40:39 +0900

    안쓰는 import삭

--- src/main/java/com/maple/loganalyzer/main/LogAnalyzerMain.java
+++ src/main/java/com/maple/loganalyzer/main/LogAnalyzerMain.java
@@ -3,14 +3,12 @@ package com.maple.loganalyzer.main;
 import java.io.IOException;
 
 import com.maple.loganalyzer.analysis.Analyzer;
-import com.maple.loganalyzer.data.OutputLog;
 import com.maple.loganalyzer.preparer.Preparer;
 import com.maple.loganalyzer.printer.TextPrinter;
 
 public class LogAnalyzerMain {
 	public static void main(String[] args) throws IOException {
 		
-
 		Preparer preparer = new Preparer();
 		preparer.prepare();
 		
